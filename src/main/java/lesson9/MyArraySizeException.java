011a676e2d1de64526fc1960e5190357949144d2
Whisp <Makkurano@gmail.com>
2021-02-28 16:21:42 +0300

    Добавил переменную

--- src/main/java/lesson9/MyArraySizeException.java
+++ src/main/java/lesson9/MyArraySizeException.java
@@ -3,14 +3,15 @@ package lesson9;
 public class MyArraySizeException extends Throwable {
 
     public static void checkArray(String[][] myArray) throws MyArraySizeException {
+        int arrLenght = 4;
 
-        if (myArray.length != 4) {
+        if (myArray.length != arrLenght) {
             System.out.println("Размер некорректен в массиве");
             throw new MyArraySizeException();
         }
 
         for (String[] insideArr : myArray) {
-            if (insideArr.length != 4) {
+            if (insideArr.length != arrLenght) {
                 System.out.println("Размер некорректен в массиве массива");
                 throw new MyArraySizeException();
             }
